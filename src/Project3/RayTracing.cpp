cbf0d772106c1d0a3d3ec2a28e7332f20be1166c
EraserMice <eameres@nycap.rr.com>
2021-10-14 22:00:47 -0400

    adding a Basic Chapter (simplified Chapter2)

--- src/Project3/RayTracing.cpp
+++ src/Project3/RayTracing.cpp
@@ -199,7 +199,7 @@ void render(const std::vector<Sphere>& spheres)
             imageBuff[y][x][0] = pixel->x * 255.0;
             imageBuff[y][x][1] = pixel->y * 255.0;
             imageBuff[y][x][2] = pixel->z * 255.0;
-            imageBuff[y][x][3] = 64.0;
+            imageBuff[y][x][3] = 255.0;
         }
     }
 #ifdef WRITE_TO_PPM
